63dd416ca9d0fecdce59509826f9edeef51bad00
Peter Murutu <murutupeter@gmail.com>
2022-07-12 00:06:47 +0300

    google oauth

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
+
 function PrivateRoute() {
-  return <div>PrivateRoute</div>;
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
 export default PrivateRoute;
