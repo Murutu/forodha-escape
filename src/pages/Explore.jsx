ed9f153e1eec8f7087e06e47c4d6d913c6ee2fbe
Peter Murutu <murutupeter@gmail.com>
2022-07-12 22:29:07 +0300

    listing page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,28 @@
+import { Link } from "react-router-dom";
+import pexels from "../assets/jpg/pexels.jpg";
+import jess from "../assets/jpg/jess.jpg";
+
 function Explore() {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <div className="pageHeader">Explore</div>
+      </header>
+
+      <main>
+        <p className="exploreCategoryHeader">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img src={pexels} alt="rent" className="exploreCategoryImg" />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sale">
+            <img src={jess} alt="sell" className="exploreCategoryImg" />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 export default Explore;
